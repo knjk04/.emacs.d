09003f671e99018d88bad78b5d6293299a13a8ac
Karan Kumar <karan.n.j.kumar@gmail.com>
2019-08-25 13:32:04 +0100

    add init.el

---
+++ init.el
@@ -0,0 +1,36 @@
+'melpa
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+;; load org-mode for org-babel
+(use-package org
+  :ensure t)
+(require 'org)
+
+(org-babel-load-file
+ (expand-file-name "config.org"
+                   user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (nlinum dashboard rainbow-delimiters nlinum-relative ido-vertical-mode magit undo-tree evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 0.9))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.8)))))
